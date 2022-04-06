6921e36da5b31abd829967450d14322c2a97e739
ShardaJuhi21 <91040711+ShardaJuhi21@users.noreply.github.com>
2022-04-06 22:11:40 +0530

    Update CurrentManagement.c

--- CurrentManagement.c
+++ CurrentManagement.c
@@ -3,6 +3,50 @@
 #include "CurrentManagement.h"
 #include <stdio.h>
 
-bool DetectRange(){
+void swap(int* xp, int* yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+ 
+void Sort(int arr[], int n)
+{
+    int i, j, min_idx;
+ 
+    // One by one move boundary of unsorted subarray
+    for (i = 0; i < n - 1; i++) {
+ 
+        // Find the minimum element in unsorted array
+        min_idx = i;
+        for (j = i + 1; j < n; j++)
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+ 
+        // Swap the found minimum element
+        // with the first element
+        swap(&arr[min_idx], &arr[i]);
+    }
+}
+ 
+// Function to print an array
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+ 
+
+bool DetectRange(int Array[10]){
+    int n = sizeof(Array) / sizeof(Array[0]);
+     
+       printf("Original array: \n");
+      printArray(Array, n);
+ 
+    Sort(arr, n);
+    printf("\nSorted array in Ascending order: \n");
+    printArray(Array, n);
 return TRUE;
 }

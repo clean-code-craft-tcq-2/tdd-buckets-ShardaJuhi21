a035595c80c6c37ce5028bd5a5b37f5fedc041e2
ShardaJuhi21 <91040711+ShardaJuhi21@users.noreply.github.com>
2022-04-06 22:56:27 +0530

    Update test_currentrange.cpp

--- test_currentrange.cpp
+++ test_currentrange.cpp
@@ -7,17 +7,17 @@ TEST_CASE("detect the ranges and output the number of readings in each range") {
   bool expectedVal = TRUE;
   bool retVal ;
   int anArray[] ={5,3,4,10,11,12};
-  GIVEN( "An Array is not sorted" ) {
+  //GIVEN( "An Array is not sorted" ) {
 
         // Validate one use case for the GIVEN object
-        WHEN( " DetectRange() is called" ) {
+       // WHEN( " DetectRange() is called" ) {
             retVal = DetectRange(anArray);
 
-            THEN( "whether the function returns true or not" ) {
+            //THEN( "whether the function returns true or not" ) {
                 REQUIRE( retVal == expectedVal );
-            }
-        }
-    }
+//             }
+//         }
+//     }
   
   
  }

db2bfc774cb38d58627e8dbee44be2e48349261e
ShardaJuhi21 <91040711+ShardaJuhi21@users.noreply.github.com>
2022-04-07 07:35:02 +0530

    Update test_currentrange.cpp

--- test_currentrange.cpp
+++ test_currentrange.cpp
@@ -39,3 +39,39 @@ TEST_CASE("detect the ranges and output the number of readings in each range") {
   
   
  }
+
+TEST_CASE("Convert in A2D") {
+  bool expectedVal = TRUE;
+  bool retVal ;
+ int 12bit_anArray[] ={5,3,4,10,11,12,7,8,9,1,2,3};
+int sizeofReadingArray = sizeof(12bit_anArray) / sizeof(12bit_anArray[0]);
+     
+  GIVEN( "An Array is 12 bit Array" ) {
+
+       // Validate one use case for the GIVEN object
+       WHEN( "Sensors() is called" ) {
+            retVal = Sensors(anArray, n);
+
+            THEN( "whether the function returns true or not" ) {
+                REQUIRE( retVal == expectedVal );
+            }
+        }
+    }
+  
+  int 10bit_Reading[] ={5,3,4,10,11,12,7,8,9,1};
+       int sizeofReadingArray = sizeof(10bit_Reading) / sizeof(10bit_Reading[0]);
+     
+   GIVEN( "An Array is sorted but it has repeated numbers" ) {
+
+       // Validate one use case for the GIVEN object
+       WHEN( " Sensors() is called" ) {
+            retVal = Sensors(10bit_Reading,sizeofReadingArray);
+
+            THEN( "whether the function returns true or not" ) {
+                REQUIRE( retVal == expectedVal );
+            }
+        }
+    }
+  
+  
+ }
